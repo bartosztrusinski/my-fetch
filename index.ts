375aec773bf2c2d6267a8882b2a7e825c9da27e0
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-07-31 21:59:57 +0200

    try fetch for get and post

--- index.ts
+++ index.ts
@@ -1,7 +1,38 @@
-import { myFetch } from './myFetch';
+import { myFetch } from './myFetch.js';
 
 const url = new URL('https://jsonplaceholder.typicode.com/posts');
 
-myFetch(url).then((response) => {
-  console.log(response);
-});
+makeGetRequest();
+makePostRequest();
+
+async function makeGetRequest() {
+  try {
+    const response = await myFetch(`${url}/1`);
+
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+
+    const data = await response.json();
+
+    console.table(data);
+  } catch (error) {
+    console.error(`Fetch error: ${error}`);
+  }
+}
+
+async function makePostRequest() {
+  try {
+    const response = await myFetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json; charset=UTF-8' },
+      body: JSON.stringify({ title: 'foo', body: 'bar', userId: 1 }),
+    });
+
+    const data = await response.json();
+
+    console.table(data);
+  } catch (error) {
+    console.error(`Fetch error: ${error}`);
+  }
+}
