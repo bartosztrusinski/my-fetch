858cdc8b1a265b1fa66fee77a09dc1fea8a292a3
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-07-31 22:45:15 +0200

    fetch using ui

--- index.ts
+++ index.ts
@@ -1,11 +1,57 @@
 import { myFetch } from './myFetch.js';
 
+type Post = {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+};
+
 const url = new URL('https://jsonplaceholder.typicode.com/posts');
 
-makeGetRequest();
-makePostRequest();
+const getPostsForm = document.querySelector('#get-posts') as HTMLFormElement;
+const createPostForm = document.querySelector(
+  '#create-post'
+) as HTMLFormElement;
+const postsEl = document.querySelector('#posts') as HTMLElement;
+const createdPostsEl = document.querySelector('#created-posts') as HTMLElement;
 
-async function makeGetRequest() {
+getPostsForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const post = await getPosts();
+
+  if (!post) {
+    postsEl.innerHTML = '<p>No post found!</p>';
+    return;
+  }
+
+  postsEl.innerHTML = `
+    <h2>${post.title}</h2>
+    <p>${post.body}</p>
+  `;
+});
+
+createPostForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(createPostForm);
+  const createdPost = await createPost(formData);
+
+  if (!createdPost) {
+    createdPostsEl.innerHTML = '<p>Could not create a post!</p>';
+    return;
+  }
+
+  createdPostsEl.innerHTML += `
+    <h2>${createdPost.title}</h2>
+    <p>${createdPost.body}</p>
+  `;
+
+  createPostForm.reset();
+});
+
+async function getPosts() {
   try {
     const response = await myFetch(`${url}/1`);
 
@@ -13,25 +59,28 @@ async function makeGetRequest() {
       throw new Error('Network response was not ok');
     }
 
-    const data = await response.json();
+    const data: Post = await response.json();
 
-    console.table(data);
+    return data;
   } catch (error) {
     console.error(`Fetch error: ${error}`);
   }
 }
 
-async function makePostRequest() {
+async function createPost(formData: FormData) {
   try {
+    const title = formData.get('title');
+    const body = formData.get('body');
+
     const response = await myFetch(url, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json; charset=UTF-8' },
-      body: JSON.stringify({ title: 'foo', body: 'bar', userId: 1 }),
+      body: JSON.stringify({ title, body }),
     });
 
-    const data = await response.json();
+    const data: Post = await response.json();
 
-    console.table(data);
+    return data;
   } catch (error) {
     console.error(`Fetch error: ${error}`);
   }
